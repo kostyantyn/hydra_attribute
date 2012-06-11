9c991b9be0e7b93797aa11fb2a0b0700d9a28e24
Kostyantyn Stepanyuk <kostya.stepanyuk@gmail.com>
2012-06-12 01:12:04 +0300

    Use mock instead anonymous Class

--- spec/hydra_attribute/active_record/relation_spec.rb
+++ spec/hydra_attribute/active_record/relation_spec.rb
@@ -1,110 +1,65 @@
 require 'spec_helper'
 
 describe HydraAttribute::ActiveRecord::Relation do
-  def record_class(loaded_associations = false)
-    Class.new do
 
-      @hydra_attributes = {'code' => :string}
-      define_singleton_method :hydra_attribute_types do
-        [:string]
-      end
-
-      define_singleton_method :hydra_attribute_names do
-        ['code']
-      end
-
-      define_singleton_method :hydra_attributes do
-        @hydra_attributes
-      end
-
-      define_method :association do |_|
-        Class.new do
-          define_singleton_method :loaded? do
-            loaded_associations
-          end
-        end
-      end
-    end
+  let(:record_class) do
+    mock(:record_class, hydra_attribute_types: [:string], hydra_attribute_names: %w(code), hydra_attributes: {'code' => :string})
   end
 
-  def relation_function(records)
-    Module.new do
-      define_method :loaded? do
-        false
-      end
-
-      define_method :select_values do
-        []
-      end
-
-      define_method :hydra_select_values do
-        []
-      end
-
-      define_method :exec_queries do
-        records
-      end
-
-      if ::ActiveRecord::VERSION::STRING.start_with?('3.1.')
-        define_method :to_a do
-          records
-        end
-      end
-
-      define_method :klass do
-        records.first.class
-      end
-
-      define_method :where do |*|
-        self
-      end
-    end
+  let(:record) do
+    mock(:record, class: record_class, association: mock(loaded?: false))
   end
 
-  let(:records)  { [record_class.new] }
-  let(:ancestor) { relation_function(records) }
-  let(:klass)    do
-    object   = Class.new.send(:include, ancestor)
-    instance = object.new
-    instance.singleton_class.send(:include, HydraAttribute::ActiveRecord::Relation)
-    instance
+  let(:loaded_record) do
+    mock(:loaded_record, class: record_class, association: mock(loaded?: true))
+  end
+
+  let(:exec_method) do
+    ::ActiveRecord::VERSION::STRING.start_with?('3.1.') ? :to_a : :exec_queries
+  end
+
+  let(:relation) do
+    mock_relation = mock(:relation, loaded?: false, select_values: [], hydra_select_values: [])
+
+    mock_relation.stub(exec_method).and_return(records)
+    mock_relation.stub(:klass).and_return(records.first.class)
+    mock_relation.stub(:where).and_return(mock_relation)
+    mock_relation
+  end
+
+  let(:records) do
+    [record]
+  end
+
+  before do
+    relation.singleton_class.send(:include, HydraAttribute::ActiveRecord::Relation)
   end
 
   describe "#exec_queries" do
-    let(:exec_queries) do
-      if ::ActiveRecord::VERSION::STRING.start_with?('3.1.')
-        :to_a
-      else
-        :exec_queries
-      end
-    end
-
     describe 'parent method return one record' do
       it 'should return one record' do
-        klass.send(exec_queries).should have(1).record
+        relation.send(exec_method).should have(1).record
       end
     end
 
     describe 'parent method returns two records' do
-      let(:records) { [record_class(loaded_associations).new, record_class(loaded_associations).new] }
-
       describe 'association models are already loaded' do
-        let(:loaded_associations) { true }
+        let(:records) { [loaded_record, loaded_record] }
 
         it 'should return two record' do
-          klass.send(exec_queries).should have(2).records
+          relation.send(exec_method).should have(2).records
         end
       end
 
       describe 'association models are not yet loaded' do
-        let(:loaded_associations) { false }
+        let(:records) { [record, record] }
 
         before do
           ::ActiveRecord::Associations::Preloader.should_receive(:new).with(records, :hydra_string_attributes).and_return(mock(run: records))
         end
 
         it 'should return two record' do
-          klass.send(exec_queries).should have(2).records
+          relation.send(exec_method).should have(2).records
         end
       end
     end
