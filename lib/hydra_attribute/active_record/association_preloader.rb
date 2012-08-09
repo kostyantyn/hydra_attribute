79ad7300211e1b82d19810043046939f24836ee5
Kostyantyn Stepanyuk <kostya.stepanyuk@gmail.com>
2012-08-10 01:46:12 +0300

    Refactoring association preloader class

--- lib/hydra_attribute/active_record/association_preloader.rb
+++ lib/hydra_attribute/active_record/association_preloader.rb
@@ -1,12 +1,12 @@
 module HydraAttribute
   module ActiveRecord
     class AssociationPreloader
-      attr_reader :relation, :records, :hashed_records
+      attr_reader :relation, :records
 
       def initialize(relation, records = [])
-        @relation       = relation
-        @records        = records
-        @hashed_records = records.each_with_object({}) { |record, hash| hash[record.id] = record }
+        @relation = relation
+        @records  = records
+        prepared_records # lock attributes
       end
 
       def self.run(relation, records)
@@ -14,60 +14,73 @@ module HydraAttribute
       end
 
       def run
-        grouped_values do |association, values, white_list_attribute_ids|
-          association.target.concat(values)
-          association.lock_for_build!(white_list_attribute_ids)
-        end
-      end
+        return if records.blank?
+        return if hydra_attribute_ids.blank?
 
-      private
+        prepared_records.keys.each_slice(in_clause_length || prepared_records.keys.length) do |entity_ids|
+          grouped_attribute_ids.each do |backend_type, hydra_attribute_ids|
+            next if hydra_attribute_ids.blank?
 
-      def grouped_values
-        grouped_attribute_ids do |type, attribute_ids|
-          association_name = AssociationBuilder.association_name(type)
-          load_values(type, attribute_ids) do |record, values|
-            yield(record.association(association_name), values, attribute_ids)
-          end
-        end
-      end
-
-      def load_values(type, attribute_ids)
-        return records.each { |record| yield(record, []) } if attribute_ids.empty?
-
-        attribute_ids.each_slice(in_clause_length || attribute_ids.length) do |sliced_attribute_ids|
-          records.each_slice(in_clause_length || records.length) do |sliced_records|
-            values = AssociationBuilder.class_name(klass, type).constantize.select([:id, :entity_id, :hydra_attribute_id, :value]).where(entity_id: sliced_records, hydra_attribute_id: sliced_attribute_ids)
-            group_records_with_values(sliced_records, values).each do |record_id, grouped_values|
-              yield(hashed_records[record_id], grouped_values)
+            hydra_attribute_ids.each_slice(in_clause_length || hydra_attribute_ids.length) do |attribute_ids|
+              value_class(backend_type).select(%w(id entity_id hydra_attribute_id value)).where(entity_id: entity_ids, hydra_attribute_id: attribute_ids).each do |model|
+                assoc = association_builder.association_name(backend_type)
+                prepared_records[model.entity_id].association(assoc).target.push(model)
+              end
             end
           end
         end
       end
 
-      def group_records_with_values(records, values)
-        hash = records.each_with_object({}) { |record, hash| hash[record.id] = [] }
-        values.each { |value| hash[value.entity_id] << value }
-        hash
+      private
+
+      def prepared_records
+        @prepared_records ||= records.each_with_object({}) do |record, hash|
+          grouped_attribute_ids.each do |backend_type, ids|
+            record.association(association_builder.association_name(backend_type)).lock_for_build!(ids)
+          end
+          hash[record.id] = record
+        end
       end
 
       def grouped_attribute_ids
-        map     = klass.hydra_attribute_backend_types.each_with_object({}) { |type, object| object[type] = [] }
-        mapping = hydra_attributes.each_with_object(map) do |hydra_attribute, mapping|
-          mapping[hydra_attribute.backend_type] << hydra_attribute.id
+        @grouped_attribute_ids ||= begin
+          map = klass.hydra_attribute_backend_types.each_with_object({}) { |type, object| object[type] = [] }
+          hydra_attributes.each_with_object(map) do |hydra_attribute, mapping|
+            mapping[hydra_attribute.backend_type] << hydra_attribute.id
+          end
         end
-        mapping.each { |type, ids| yield(type, ids) }
       end
 
       def hydra_attributes
-        if attribute_limit?
+        @hydra_attributes ||= if attribute_limit?
           relation.hydra_select_values.map { |name| klass.hydra_attribute(name) }
         else
           klass.hydra_attributes
         end
       end
 
+      def hydra_attribute_ids
+        @hydra_attribute_ids ||= hydra_attributes.map(&:id)
+      end
+
+      def hydra_attribute_backend_types
+        @hydra_attribute_backend_types ||= hydra_attributes.map(&:backend_type).uniq
+      end
+
       def attribute_limit?
         relation.select_values.any? or relation.hydra_select_values.any?
+      end
+
+      def value_class(backend_type)
+        instance_variable_get(:"@#{backend_type}_class") || instance_variable_set(:"@#{backend_type}_class", association_builder.class_name(klass, backend_type).constantize)
+      end
+
+      def association_builder
+        AssociationBuilder
+      end
+
+      def connection
+        relation.connection
       end
 
       def klass
@@ -75,7 +88,7 @@ module HydraAttribute
       end
 
       def in_clause_length
-        klass.connection.in_clause_length
+        connection.in_clause_length
       end
     end
   end
