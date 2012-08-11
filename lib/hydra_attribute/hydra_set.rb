7544468ab84316f37d96e4c8768e1b6b4a09ec6a
Kostyantyn Stepanyuk <kostya.stepanyuk@gmail.com>
2012-08-11 18:45:33 +0300

    Add HydraSet model

---
+++ lib/hydra_attribute/hydra_set.rb
@@ -0,0 +1,16 @@
+require 'active_support/core_ext/object/with_options'
+
+module HydraAttribute
+  class HydraSet < ActiveRecord::Base
+    self.table_name = 'hydra_sets'
+
+    attr_accessible :name
+
+    has_and_belongs_to_many :hydra_attributes, join_table: 'hydra_attribute_sets', class_name: 'HydraAttribute::HydraAttribute', conditions: proc { {hydra_attributes: {entity_type: entity_type}} }
+
+    with_options presence: true do |klass|
+      klass.validates :entity_type,  inclusion: { in: lambda { |attr| [(attr.entity_type.constantize.name rescue nil)] } }
+      klass.validates :name,         uniqueness: { scope: :entity_type }
+    end
+  end
+end
