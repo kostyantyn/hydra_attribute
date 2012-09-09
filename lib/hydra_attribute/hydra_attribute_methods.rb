bcd3bdae191030e041e971789a5456ad72b1d61c
Kostyantyn Stepanyuk <kostya.stepanyuk@gmail.com>
2012-09-09 19:56:45 +0300

    Add comments for HydraAttributeMethods

--- lib/hydra_attribute/hydra_attribute_methods.rb
+++ lib/hydra_attribute/hydra_attribute_methods.rb
@@ -1,15 +1,37 @@
 module HydraAttribute
+
+  # See HydraAttribute::HydraAttributeMethods::ClassMethods for documentation.
   module HydraAttributeMethods
     extend ActiveSupport::Concern
 
     module ClassMethods
       extend Memoize
 
+      # Returns prepared <tt>ActiveRecord::Relation</tt> object with preloaded attributes for current entity.
+      # This method is cacheable, so just one request per entity will be sent to the database.
+      #
+      #   Product.hydra_attributes                                               # ActiveRecord::Relation
+      #   Product.hydra_attributes.map(&:name)                                   # ["title", "color", ...]
+      #   Product.hydra_attributes.create(name: 'title', backend_type: 'string') # Create and return new attribute
+      #   Product.hydra_attributes.each(&:destroy)                               # Remove all attributes
       def hydra_attributes
         HydraAttribute.where(entity_type: base_class.model_name)
       end
       hydra_memoize :hydra_attributes
 
+      # Finds <tt>HydraAttribute::HydraAttribute</tt> model by +id+ or +name+. Returns +nil+ for unknown attribute.
+      # This method is cacheable per +identifier+.
+      # It's better to use it instead of manually searching from <tt>hydra_attributes</tt> collection.
+      #
+      #   Product.hydra_attribute('name') # HydraAttribute::HydraAttribute
+      #   Product.hydra_attribute('name') # Returns HydraAttribute::HydraAttribute from cache
+      #   Product.hydra_attribute(10)     # HydraAttribute::HydraAttribute
+      #   Product.hydra_attribute('FAKE') # nil
+      #
+      #   # It's better to use this method
+      #   Product.hydra_attribute('name')
+      #   # instead of manually searching
+      #   Product.hydra_attributes.detect { |attr| attr.name == 'name' || attr.id == 'id' }
       def hydra_attribute(identifier)
         hydra_attributes.find do |hydra_attribute|
           hydra_attribute.id == identifier || hydra_attribute.name == identifier
@@ -17,11 +39,40 @@ module HydraAttribute
       end
       hydra_memoize :hydra_attribute
 
+      # Returns unique array of attribute backend types.
+      # This method is cacheable therefore, the definition of values is carried out only once.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer')
+      #
+      #   Product.hydra_attribute_backend_types # ["string", "integer"]
       def hydra_attribute_backend_types
         hydra_attributes.map(&:backend_type).uniq
       end
       hydra_memoize :hydra_attribute_backend_types
 
+      ##
+      # :method: hydra_attribute_ids
+      # Returns array of attribute ids.
+      # This method is cacheable therefore, the definition of values is carried out only once.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')     # 1
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')     # 2
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer')  # 3
+      #
+      #   Product.hydra_attribute_ids # [1, 2, 3]
+
+      ##
+      # :method: hydra_attribute_names
+      # Returns array of attribute names.
+      # This method is cacheable therefore, the definition of values is carried out only once.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer')
+      #
+      #   Product.hydra_attribute_names # ["one", "two", "three"]
       %w(id name).each do |prefix|
         module_eval <<-EOS, __FILE__, __LINE__ + 1
           def hydra_attribute_#{prefix}s
@@ -31,11 +82,39 @@ module HydraAttribute
         EOS
       end
 
+      # Groups current attributes by backend type.
+      # This method is cacheable therefore, the group operation is carried out only once.
+      #
+      #   Product.hydra_attributes_by_backend_type
+      #   # {"string" => [...], "integer" => [...]}
       def hydra_attributes_by_backend_type
         hydra_attributes.group_by(&:backend_type)
       end
       hydra_memoize :hydra_attributes_by_backend_type
 
+      ##
+      # :method: hydra_attribute_ids_by_backend_type
+      # Group attribute ids by backend type.
+      # This method is cacheable therefore, the group operation is carried out only once.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')    # 1
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')    # 2
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer') # 3
+      #
+      #   Product.hydra_attribute_ids_by_backend_type
+      #   # {"string" => [1, 2], "integer" => [3]}
+
+      ##
+      # :method: hydra_attribute_names_by_backend_type
+      # Group attribute names by backend type.
+      # This method is cacheable therefore, the group operation is carried out only once.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer')
+      #
+      #   Product.hydra_attribute_names_by_backend_type
+      #   # {"string" => ["one", "two"], "integer" => ["three"]}
       %w(id name).each do |prefix|
         module_eval <<-EOS, __FILE__, __LINE__ + 1
           def hydra_attribute_#{prefix}s_by_backend_type
@@ -48,11 +127,45 @@ module HydraAttribute
         EOS
       end
 
+      # Returns array of attributes for passed backend type.
+      # If no attributes, returns blank array.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer')
+      #
+      #   Product.hydra_attributes_for_backend_type('string')
+      #   # [<HydraAttribute::HydraAttribute id: 1, name: "one", ...>, <HydraAttribute::HydraAttribute id: 2, name: "two", ...>]
+      #
+      #   Product.hydra_attributes_for_backend_type('float') # []
       def hydra_attributes_for_backend_type(backend_type)
         hydra_attributes = hydra_attributes_by_backend_type[backend_type]
         hydra_attributes.nil? ? [] : hydra_attributes
       end
 
+      ##
+      # :method: hydra_attribute_ids_for_backend_type(backend_type)
+      # Returns array of attribute ids for passed backend type.
+      # If no attributes, returns blank array.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')    # 1
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')    # 2
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer') # 3
+      #
+      #   Product.hydra_attributes_for_backend_type('string') # [1, 2]
+      #   Product.hydra_attributes_for_backend_type('float')  # []
+
+      ##
+      # :method: hydra_attribute_names_for_backend_type(backend_type)
+      # Returns array of attribute names for passed backend type.
+      # If no attributes, returns blank array.
+      #
+      #   Product.hydra_attributes.create(name: 'one', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'two', backend_type: 'string')
+      #   Product.hydra_attributes.create(name: 'three', backend_type: 'integer')
+      #
+      #   Product.hydra_attributes_for_backend_type('string') # ["one", "two"]
+      #   Product.hydra_attributes_for_backend_type('float')  # []
       %w(id name).each do |prefix|
         module_eval <<-EOS, __FILE__, __LINE__ + 1
           def hydra_attribute_#{prefix}s_for_backend_type(backend_type)
