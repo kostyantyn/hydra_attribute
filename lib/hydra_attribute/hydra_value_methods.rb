5af7101b07c8ce78ae80fd30dece4a75dd590f90
Kostyantyn Stepanyuk <kostya.stepanyuk@gmail.com>
2012-09-10 21:27:16 +0300

    Add YARD documentation for HydraValueMethods

--- lib/hydra_attribute/hydra_value_methods.rb
+++ lib/hydra_attribute/hydra_value_methods.rb
@@ -3,10 +3,17 @@ module HydraAttribute
     extend ActiveSupport::Concern
 
     module ClassMethods
+      # Clear cache
+      #
+      # @return [NilClass]
       def clear_hydra_value_cache!
       end
     end
 
+    # Returns the association instance for the given backend type attribute name
+    #
+    # @param backend_type [String] one of the HydraAttribute::SUPPORTED_BACKEND_TYPES values
+    # @return [ActiveRecord::Associations::Association]
     def hydra_value_association(backend_type)
       association(::HydraAttribute::AssociationBuilder.association_name(backend_type))
     end
