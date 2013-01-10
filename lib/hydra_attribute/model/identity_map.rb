69862048bb47b4d6f11324c7393d2a0bf4d9fb58
Kostyantyn <kostya.stepanyuk@gmail.com>
2013-01-10 22:22:30 +0200

    Add possibility for creating nested indentity map objects

--- lib/hydra_attribute/model/identity_map.rb
+++ lib/hydra_attribute/model/identity_map.rb
@@ -29,6 +29,24 @@ module HydraAttribute
         def cache(key, value = nil, &block)
           identity_map.cache(key, value, &block)
         end
+
+        # Generates nested cache keys
+        #
+        # @param [Array<Symbol>] cache_keys
+        # @return [NilClass]
+        def nested_cache_keys(*cache_keys)
+          cache_keys.each do |cache_key|
+            instance_eval <<-EOS, __FILE__, __LINE__ + 1
+              def #{cache_key}_identity_map                                             # def name_identity_map
+                identity_map.cache(:#{cache_key}) { ::HydraAttribute::IdentityMap.new } #   identity_map(:name) { ::HydraAttribute::identityMap.new }
+              end                                                                       # end
+
+              def #{cache_key}_cache(key, value = nil, &block)                          # def name_cache(key, value = nil, &block)
+                #{cache_key}_identity_map.cache(key, value, &block)                     #   name_identity_map.cache(key, value, &block)
+              end                                                                       # end
+            EOS
+          end
+        end
       end
     end
   end
--- spec/hydra_attribute/model/identity_map_spec.rb
+++ spec/hydra_attribute/model/identity_map_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe HydraAttribute::Model::IdentityMap do
-  before(:all) do
+  before do
     Object.const_set(:ExampleClass, Class.new)
     ExampleClass.send(:include, HydraAttribute::Model::IdentityMap)
   end
 
-  after(:all) do
+  after do
     Object.send(:remove_const, :ExampleClass)
   end
 
@@ -34,6 +34,48 @@ describe HydraAttribute::Model::IdentityMap do
 
       ExampleClass.cache(:b) { 2 }
       ExampleClass.identity_map.cache(:b) { 3 }.should be(2)
+    end
+  end
+
+  describe '.nested_cache_keys' do
+    it 'should generate nested identity map storage' do
+      ExampleClass.nested_cache_keys(:one, :two)
+
+      ExampleClass.one_identity_map.should be_a_kind_of(HydraAttribute::IdentityMap)
+      ExampleClass.two_identity_map.should be_a_kind_of(HydraAttribute::IdentityMap)
+    end
+
+    it 'should generate different identity map storage' do
+      ExampleClass.nested_cache_keys(:one, :two)
+
+      im1 = ExampleClass.one_identity_map
+      im2 = ExampleClass.two_identity_map
+
+      im1.should_not be(im2)
+    end
+
+    it 'should store generated identity map storage into existed identity map for class' do
+      ExampleClass.nested_cache_keys(:one, :two)
+
+      im1 = ExampleClass.one_identity_map
+      im2 = ExampleClass.two_identity_map
+
+      ExampleClass.identity_map[:one].should be(im1)
+      ExampleClass.identity_map[:two].should be(im2)
+    end
+
+    it 'should generate proxy cache method for identity map' do
+      ExampleClass.nested_cache_keys(:one, :two)
+
+      ExampleClass.one_cache(:a, 1)
+      ExampleClass.two_cache(:a, 2)
+      ExampleClass.one_cache(:b) { 3 }
+      ExampleClass.two_cache(:b) { 4 }
+
+      ExampleClass.one_identity_map[:a].should be(1)
+      ExampleClass.two_identity_map[:a].should be(2)
+      ExampleClass.one_identity_map[:b].should be(3)
+      ExampleClass.two_identity_map[:b].should be(4)
     end
   end
 end
