13f14ded64c8f28105f23d045e10f3aff25550f7
Kostyantyn <kostya.stepanyuk@gmail.com>
2014-03-23 18:00:27 +0100

    Remove #read_attribute
    It was redefined in Rails4 and now it's hard to patch it correctly.
    It's an internal AR method, so we don't need to implement it in hydra_attribute.

--- lib/hydra_attribute/active_record/attribute_methods.rb
+++ lib/hydra_attribute/active_record/attribute_methods.rb
@@ -17,25 +17,11 @@ module HydraAttribute
         super.merge(hydra_attributes_before_type_cast)
       end
 
-      # Read type cast attribute value by its name
-      #
-      # @param [String,Symbol] name
-      # @return [Hash]
-      def read_attribute(name)
-        name = name.to_s
-        if hydra_attributes.has_key?(name)
-          hydra_attributes[name]
-        else
-          super
-        end
-      end
-
       # Assigns attributes to the model
       #
       # @param [Hash] new_attributes
-      # @param [Hash] options
       # @return [NilClass]
-      def assign_attributes(new_attributes, options = {})
+      def assign_attributes(new_attributes)
         if new_attributes[:hydra_set_id]
           # set :hydra_set_id attribute as a last attribute to avoid HydraAttribute::HydraSet::MissingAttributeInHydraSetError error
           new_attributes[:hydra_set_id] = new_attributes.delete(:hydra_set_id)
--- spec/hydra_attribute/active_record/attribute_methods_spec.rb
+++ spec/hydra_attribute/active_record/attribute_methods_spec.rb
@@ -1,37 +1,6 @@
 require 'spec_helper'
 
 describe HydraAttribute::ActiveRecord::AttributeMethods do
-  describe '#read_attribute' do
-    before do
-      Product.hydra_attributes.create(name: 'code',  backend_type: 'string',  default_value: 'abc')
-    end
-
-    let(:product) { Product.new }
-
-    it 'should return value of static attribute' do
-      product.name = 'one'
-      product.read_attribute(:name).should == 'one'
-    end
-
-    it 'should return default hydra attribute value' do
-      product.read_attribute(:code).should == 'abc'
-    end
-
-    it 'should return the value of hydra attribute' do
-      product.code = 'second'
-      product.read_attribute(:code).should == 'second'
-    end
-
-    it 'should return nil for unknown attribute' do
-      product.read_attribute(:unknown).should be_nil
-    end
-
-    it 'should return nil if hydra attribute does not exist in hydra set' do
-      product.hydra_set_id = Product.hydra_sets.create(name: 'default').id
-      product.read_attribute(:name).should be_nil
-    end
-  end
-
   describe '#column_for_attribute' do
     let!(:hydra_attribute) { Product.hydra_attributes.create(name: 'code',  backend_type: 'string',  default_value: 'abc') }
     let(:product)          { Product.new }
